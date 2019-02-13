13cfeb282b04e61184fa38d188e07d2a076d7663
GotoShuhei <shuhei-g@m-craft.com>
2019-02-13 18:37:40 +0900

    ピクセル操作関係追加

--- include/yatools/image/yatools.image.colorbuffer.h
+++ include/yatools/image/yatools.image.colorbuffer.h
@@ -55,27 +55,101 @@ namespace yatools
 			* @}
 			*/
 			/**
-			* @name 変換
-			* @{
-			*/
-			/**
-			* @brife フォーマット変換
-			* @param[in] format				新しいフォーマット
-			* @returns						新しいフォーマットに変換されたカラーバッファ
-			* @details
-			*	新しいフォーマットにカラーバッファを変換します。
-			*/
-			ColorBuffer* Convert( ColorFormat format );
-
-			/**
-			* @}
-			*/
-			/**
 			* @name ピクセル操作
 			* @{
 			*/
+			/**
+			* @brief ピクセルサイズ取得
+			* @returns						1ピクセル当たりのバイト数
+			* @details
+			*	カラーバッファの1ピクセル当たりのバイト数を取得します。
+			*/
+			uint64 GetPixelSize( void );
 
-			bool SetPixel( uint32 x, uint32 y, float r, float g, float b, float a = 1.0f );
+			/**
+			* @brief ピクセル設定
+			* @param[in] x, y				座標
+			* @param[in] pixel				設定するカラーが格納されている領域の先頭ポインタ
+			* @param[in] length				colorが指すポインタの領域サイズ
+			* @retval true					設定に成功
+			* @retval false					設定に失敗
+			* @details
+			*	指定座標のピクセルに対してピクセルカラーを設定します。
+			*/
+			bool SetPixel( uint32 x, uint32 y, uintptr pixel, uint64 length );
+
+			/**
+			* @brief ピクセル設定
+			* @param[in] x, y				設定開始先頭座標
+			* @param[in] pixels				設定するカラーが格納されている領域の先頭ポインタ
+			* @param[in] length				ピクセルの設定範囲
+			* @retval true					設定に成功
+			* @retval false					設定に失敗
+			* @details
+			*	x, yで指定される座標のピクセルを先頭とし、length/GetPixelSize()個数分のピクセルをpixelsの内容で変更します。
+			*/
+			bool SetPixels( uint32 x, uint32 y, uintptr pixels, uint64 length );
+
+			/**
+			* @brief ピクセルアドレス取得
+			* @param[in] x, y				座標
+			* @returns						座標のピクセルが格納されている領域の先頭ポインタ
+			* @retval NULL					取得に失敗
+			* @details
+			*	指定座標のピクセルが格納されている領域の先頭ポインタを取得します。	\n
+			*	取得に成功した場合、少なくとも「戻り値 + GetPixelSize()」の領域は有効です。
+			*/
+			uintptr GetPixelAddress( uint32 x, uint32 y );
+
+			/**
+			* @brief ピクセルブロック設定
+			* @param[in] x, y				ブロック左上座標
+			* @param[in] width, height		書き換えブロックサイズ
+			* @param[in] blocks				書き換えブロック内容を保持する領域の先頭ポインタ
+			* @param[in] length				blocksの領域サイズ
+			* @retval true					成功
+			* @retval false					失敗
+			* @details
+			*	カラーバッファ内の矩形領域を設定します。
+			*/
+			bool SetPixelBlock( uint32 x, uint32 y, uint32 width, uint32 height, uintptr blocks, uint64 length );
+
+			/**
+			* @brief ピクセルブロック設定
+			* @param[in] x, y				ブロック左上座標
+			* @param[in] blockBuffer		書き換える内容を保持するカラーバッファ	\n
+			*								カラーフォーマットが同一である必要があります。
+			* @retval true					成功
+			* @retval false					失敗
+			* @details
+			*	カラーバッファ内の矩形領域を設定します。
+			*/
+			bool SetPixelBlock( uint32 x, uint32 y, ColorBuffer* blockBuffer );
+
+			/**
+			* @brief ピクセルブロック取得
+			* @param[in] x, y				取得開始左上座標
+			* @param[in] width, height		取得横幅、縦幅
+			* @returns						ピクセルブロック全体のカラーバッファ
+			* @retval NULL					失敗
+			* @details
+			*	カラーバッファ内の矩形情報を取得します。
+			*/
+			ColorBuffer* GetPixelBlock( uint32 x, uint32 y, uint32 width, uint32 height );
+
+			/**
+			* @brief ピクセルブロック取得
+			* @param[in] x, y				取得開始左上座標
+			* @param[in] width, height		取得横幅、縦幅
+			* @param[in] blockBuffer		ピクセルブロック格納先
+			* @param[in] length				blockBufferの長さ	\n
+			*								最低でもwidth * height * GetPixelSize()が必要です。
+			* @retval true					成功
+			* @retval false					失敗
+			* @details
+			*	カラーバッファ内の矩形情報を取得します。
+			*/
+			bool GetPixelBlock( uint32 x, uint32 y, uint32 width, uint32 height, uintptr blockBuffer, uint64 length );
 
 			/**
 			* @}
@@ -120,7 +194,7 @@ namespace yatools
 			uint32			m_Height;			//!< 縦幅
 			uintptr			m_Buffer;			//!< バッファ
 			uint64			m_BufferSize;		//!< バッファサイズ
-			uint			m_PixelSize;		//!< 1ピクセルあたりのサイズ
+			uint64			m_PixelSize;		//!< 1ピクセルあたりのサイズ
 
 		};
 
--- source/image/ColorBuffer.cpp
+++ source/image/ColorBuffer.cpp
@@ -119,5 +119,116 @@ ColorBuffer::~ColorBuffer( void )
 }
 
 
+//-------------------------------------
+// ピクセル操作
+//-------------------------------------
+//-------------------------------------
+// ピクセルサイズ取得
+//
+uint64 ColorBuffer::GetPixelSize( void )
+{
+	return m_PixelSize;
+}
+
+//-------------------------------------
+// ピクセル設定
+//
+bool ColorBuffer::SetPixel( uint32 x, uint32 y, uintptr pixel, uint64 length )
+{
+	if( m_PixelSize <= length )
+	{
+		uintptr address = GetPixelAddress( x, y );
+		if( address != NULL && (address + m_PixelSize) < (m_Buffer + m_BufferSize) )
+		{
+			memcpy( (void*)address, (void*)pixel, m_PixelSize );
+			return true;
+		}
+	}
+	return false;
+}
+
+//-------------------------------------
+// ピクセル設定
+//
+bool ColorBuffer::SetPixels( uint32 x, uint32 y, uintptr pixels, uint64 length )
+{
+	uintptr address = GetPixelAddress( x, y );
+	if( address != NULL )
+	{
+		uint64 count = length / m_PixelSize;
+		if( count > 0 && address + (count * m_PixelSize) < (m_Buffer + m_BufferSize) )
+		{
+			memcpy( (void*)address, (void*)pixels, count * m_PixelSize );
+			return true;
+		}
+	}
+	return false;
+}
+
+//-------------------------------------
+// ピクセルアドレス取得
+//
+uintptr ColorBuffer::GetPixelAddress( uint32 x, uint32 y )
+{
+	if( x < m_Width && y < m_Height )
+	{
+		return m_Buffer + ((x + (m_Width * y)) * m_PixelSize);
+	}
+	return NULL;
+}
+
+//-------------------------------------
+// ピクセルブロック設定
+//
+bool ColorBuffer::SetPixelBlock( uint32 x, uint32 y, uint32 width, uint32 height, uintptr blocks, uint64 length )
+{
+	uintptr address = GetPixelAddress( x, y );
+	if( address != NULL )
+	{
+		if( x + width < m_Width && y + height < m_Height && width * height * m_PixelSize <= length )
+		{
+			uint64 rowlength = m_Width * m_PixelSize;
+			if( address + (rowlength * height) + (width * m_PixelSize) < m_Buffer + m_BufferSize )
+			{
+				uint64 blockrowlength = width * m_PixelSize;
+				for( uint32 i = 0; i < height; i++ )
+				{
+					address = GetPixelAddress( x, y + i );
+					memcpy( (void*)address, (void*)blocks, blockrowlength );
+					blocks += blockrowlength;
+				}
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+//-------------------------------------
+// ピクセルブロック設定
+//
+bool ColorBuffer::SetPixelBlock( uint32 x, uint32 y, ColorBuffer* blockBuffer )
+{
+	if( blockBuffer->m_Format != m_Format )
+	{
+		return false;
+	}
+	return SetPixelBlock( x, y, m_Width, m_Height, m_Buffer, m_BufferSize );
+}
+
+//-------------------------------------
+// ピクセルブロック取得
+//
+ColorBuffer* ColorBuffer::GetPixelBlock( uint32 x, uint32 y, uint32 width, uint32 height )
+{
+	if( x + width < m_Width && y + height < m_Height )
+	{
+		ColorBuffer* block = Create( m_Format, width, height );
+
+	}
+	return NULL;
+}
+
+
 
 } }
